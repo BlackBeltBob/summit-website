81b16d244a616748427e12f7b102e5aa553837e2
bobwansink <bob@openhealthhub.com>
2026-05-05 14:37:12 +0200

    Switch to hash router.

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import ProductPage from "./ProductPage";
 import PrivacyPage from "./PrivacyPage";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<ProductPage />} />
         <Route path="/privacy" element={<PrivacyPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
