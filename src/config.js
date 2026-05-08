8f69688269a83aa51a04704ad735707249833f75
BlackBeltBob <BlackBeltBob@users.noreply.github.com>
2026-05-08 15:05:21 +0200

    Update config.js
    change order of features

--- src/config.js
+++ src/config.js
@@ -28,10 +28,10 @@ export const APP = {
 
   featureDescription: "Our Strenghts",
   features: [
+    { icon: "🛠️", title: "Everyone", body: "Easy registration, just invite your friends to join you." },
     { icon: "⚡", title: "Easily", body: "Tell us where you want to go, and we'll plot a course." },
     { icon: "🔒", title: "Safely", body: "Define your limits, and we'll avoid challenges too great for you." },
     { icon: "🌍", title: "Everywhere", body: "Select locations from a list of slopes, restaurants, and bars." },
-    { icon: "🛠️", title: "Everyone", body: "Easy registration, just invite your friends to join you." },
   ],
 
   footer: {
